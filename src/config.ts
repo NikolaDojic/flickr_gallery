ec5370183bf4a42ecb3f41dfad0d2a42a7233a32
Nikola Dojic <nicolladojic@gmail.com>
2022-11-22 01:11:23 +0100

    minor change

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,6 @@ export const baseUrl = "https://api.flickr.com/services/rest/";
 // that way, our api key would stay unexposed
 export const apiKey = "2870d1b8d52c470470bfd96384909f9e";
 
-// this should be acquired from a flickr login
-export const userId = "197041583@N08";
-
 export const flickrMethods = {
   getRecent: "flickr.photos.getRecent",
 };
